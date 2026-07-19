71226139867cba48b75f19e558bae4ae0730c421
Nihal V <nihalsharan31@gmail.com>
2026-07-19 11:44:36 +0530

    Initial project setup

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="InsightIQ",
+    page_icon="📊",
+    layout="wide"
+)
+
+st.title("📊 InsightIQ")
+
+st.subheader("AI Powered Retail Analytics Platform")
+
+st.write("Welcome to InsightIQ!")
+
+st.success("Project Setup Completed Successfully 🚀")
